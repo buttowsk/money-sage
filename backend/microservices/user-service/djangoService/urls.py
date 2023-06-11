6b873183451289296065f82bb297a9134edc1bfa
buttowsk <vinicius_nunes0104@hotmail.com>
2023-06-11 00:56:58 -0300

    token blacklist

--- backend/microservices/user-service/djangoService/urls.py
+++ backend/microservices/user-service/djangoService/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from expenses.urls import urlpatterns as expenses_urls
+from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.social.urls')),
+    path('auth/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('expenses/', include(expenses_urls)),
 ]
